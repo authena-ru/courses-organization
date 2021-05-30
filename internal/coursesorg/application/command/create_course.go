adffc362315a71eee62ca0dfcc9b2c12a747c359
Dmitry Kotukhov <kotukhov.da@reliab.tech>
2021-05-30 15:00:49 +0300

    Create CreateCourse -command and -handler

---
+++ internal/coursesorg/application/command/create_course.go
@@ -0,0 +1,47 @@
+package command
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"github.com/authena-ru/courses-organization/internal/coursesorg/domain/course"
+)
+
+type CreateCourseCommand struct {
+	Creator       course.Academic
+	CourseStarted bool
+	CourseTitle   string
+	CoursePeriod  course.Period
+}
+
+type CreateCourseHandler struct {
+	coursesRepository coursesRepository
+}
+
+func NewCreateCourseHandler(repository coursesRepository) CreateCourseHandler {
+	if repository == nil {
+		panic("coursesRepository is nil")
+	}
+	return CreateCourseHandler{coursesRepository: repository}
+}
+
+// Handle is handler of create course command.
+// Creates course, returns id of new brand course and error
+func (h CreateCourseHandler) Handle(ctx context.Context, cmd CreateCourseCommand) (string, error) {
+	courseID := uuid.NewString()
+	crs, err := course.NewCourse(course.CreationCourseParams{
+		ID:      courseID,
+		Creator: cmd.Creator,
+		Title:   cmd.CourseTitle,
+		Period:  cmd.CoursePeriod,
+		Started: cmd.CourseStarted,
+	})
+	if err != nil {
+		return "", err
+	}
+	if err := h.coursesRepository.AddCourse(ctx, crs); err != nil {
+		return "", err
+	}
+	return courseID, nil
+}
--- internal/coursesorg/go.mod
+++ internal/coursesorg/go.mod
@@ -3,6 +3,7 @@ module github.com/authena-ru/courses-organization/internal/coursesorg
 go 1.16
 
 require (
+	github.com/google/uuid v1.2.0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
 )
