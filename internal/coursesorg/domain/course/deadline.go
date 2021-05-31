76380b17e3048fa6aa9d1fb362501e65638a899a
djerys <kotukhov99@gmail.com>
2021-05-31 12:51:06 +0300

    Add deadline for task

---
+++ internal/coursesorg/domain/course/deadline.go
@@ -0,0 +1,59 @@
+package course
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+type Deadline struct {
+	excellentGradeTime time.Time
+	goodGradeTime      time.Time
+}
+
+var (
+	ErrZeroExcellentGradeTime      = errors.New("zero excellent grade time")
+	ErrZeroGoodGradeTime           = errors.New("zero good grade time")
+	ErrExcellentGradeTimeAfterGood = errors.New("excellent grade time after good")
+)
+
+func NewDeadline(excellentGradeTime time.Time, goodGradeTime time.Time) (Deadline, error) {
+	if excellentGradeTime.IsZero() {
+		return Deadline{}, ErrZeroExcellentGradeTime
+	}
+	if goodGradeTime.IsZero() {
+		return Deadline{}, ErrZeroGoodGradeTime
+	}
+	if excellentGradeTime.After(goodGradeTime) {
+		return Deadline{}, ErrExcellentGradeTimeAfterGood
+	}
+	return Deadline{
+		excellentGradeTime: excellentGradeTime,
+		goodGradeTime:      goodGradeTime,
+	}, nil
+}
+
+func MustNewDeadline(excellentGradeTime time.Time, goodGradeTime time.Time) Deadline {
+	deadline, err := NewDeadline(excellentGradeTime, goodGradeTime)
+	if err != nil {
+		panic(err)
+	}
+	return deadline
+}
+
+func (d Deadline) GoodGradeTime() time.Time {
+	return d.goodGradeTime
+}
+
+func (d Deadline) ExcellentGradeTime() time.Time {
+	return d.excellentGradeTime
+}
+
+func (d Deadline) IsZero() bool {
+	return d == Deadline{}
+}
+
+func (d Deadline) String() string {
+	return fmt.Sprintf("excellent — %s, good — %s", d.excellentGradeTime, d.goodGradeTime)
+}
---
+++ internal/coursesorg/domain/course/deadline_test.go
@@ -0,0 +1,97 @@
+package course_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+
+	"github.com/authena-ru/courses-organization/internal/coursesorg/domain/course"
+)
+
+func TestNewDeadline(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		Name               string
+		ExcellentGradeTime time.Time
+		GoodGradeTime      time.Time
+		ExpectedErr        error
+	}{
+		{
+			Name:               "valid_time_for_creation_deadline",
+			ExcellentGradeTime: time.Date(2023, time.September, 22, 0, 0, 0, 0, time.Local),
+			GoodGradeTime:      time.Date(2023, time.October, 02, 0, 0, 0, 0, time.Local),
+			ExpectedErr:        nil,
+		},
+		{
+			Name:               "zero_excellent_grade_time",
+			ExcellentGradeTime: time.Time{},
+			GoodGradeTime:      time.Date(2021, time.November, 01, 0, 0, 0, 0, time.Local),
+			ExpectedErr:        course.ErrZeroExcellentGradeTime,
+		},
+		{
+			Name:               "zero_good_grade_time",
+			ExcellentGradeTime: time.Date(2022, time.April, 02, 0, 0, 0, 0, time.Local),
+			GoodGradeTime:      time.Time{},
+			ExpectedErr:        course.ErrZeroGoodGradeTime,
+		},
+		{
+			Name:               "excellent_grade_time_after_good",
+			ExcellentGradeTime: time.Date(2022, time.September, 28, 0, 0, 0, 0, time.Local),
+			GoodGradeTime:      time.Date(2022, time.September, 01, 0, 0, 0, 0, time.Local),
+			ExpectedErr:        course.ErrExcellentGradeTimeAfterGood,
+		},
+	}
+
+	for i := range testCases {
+		c := testCases[i]
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+
+			deadline, err := course.NewDeadline(c.ExcellentGradeTime, c.GoodGradeTime)
+
+			if c.ExpectedErr != nil {
+				require.Error(t, err)
+				require.True(t, errors.Is(err, c.ExpectedErr))
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, c.ExcellentGradeTime, deadline.ExcellentGradeTime())
+			require.Equal(t, c.GoodGradeTime, deadline.GoodGradeTime())
+		})
+	}
+}
+
+func TestDeadline_IsZero(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		Name         string
+		Deadline     course.Deadline
+		ShouldBeZero bool
+	}{
+		{
+			Name:         "should_be_zero",
+			Deadline:     course.Deadline{},
+			ShouldBeZero: true,
+		},
+		{
+			Name: "should_not_be_zero",
+			Deadline: course.MustNewDeadline(
+				time.Date(2021, time.October, 02, 0, 0, 0, 0, time.Local),
+				time.Date(2021, time.October, 12, 0, 0, 0, 0, time.Local),
+			),
+			ShouldBeZero: false,
+		},
+	}
+
+	for i := range testCases {
+		c := testCases[i]
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+
+			require.Equal(t, c.ShouldBeZero, c.Deadline.IsZero())
+		})
+	}
+}
