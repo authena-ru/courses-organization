872bda59638809b2caacf1f4b843b3de5053c6cf
djerys <kotukhov99@gmail.com>
2021-06-01 00:04:34 +0300

    Add test data for auto code checking task

---
+++ internal/coursesorg/domain/course/test_data.go
@@ -0,0 +1,48 @@
+package course
+
+import "github.com/pkg/errors"
+
+type TestData struct {
+	inputData  string
+	outputData string
+}
+
+const testDataMaxLen = 1000
+
+var (
+	ErrTestInputDataTooLong  = errors.New("test input data too long")
+	ErrTestOutputDataTooLong = errors.New("test output data too long")
+)
+
+func NewTestData(inputData, outputData string) (TestData, error) {
+	if len(inputData) > testDataMaxLen {
+		return TestData{}, ErrTestInputDataTooLong
+	}
+	if len(outputData) > testDataMaxLen {
+		return TestData{}, ErrTestOutputDataTooLong
+	}
+	return TestData{
+		inputData:  inputData,
+		outputData: outputData,
+	}, nil
+}
+
+func MustNewTestData(inputData, outputData string) TestData {
+	td, err := NewTestData(inputData, outputData)
+	if err != nil {
+		panic(err)
+	}
+	return td
+}
+
+func (td TestData) InputData() string {
+	return td.inputData
+}
+
+func (td TestData) OutputData() string {
+	return td.outputData
+}
+
+func (td TestData) IsZero() bool {
+	return td == TestData{}
+}
