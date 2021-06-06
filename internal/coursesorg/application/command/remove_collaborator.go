26367f00faeebde37d4aba31356cd09f94264c50
Dmitry Kotukhov <kotukhov.da@reliab.tech>
2021-06-06 23:03:22 +0300

    Add remove collaborator command handler

---
+++ internal/coursesorg/application/command/remove_collaborator.go
@@ -0,0 +1,38 @@
+package command
+
+import (
+	"context"
+	"github.com/authena-ru/courses-organization/internal/coursesorg/domain/course"
+)
+
+type RemoveCollaboratorCommand struct {
+	Teacher        course.Academic
+	CourseID       string
+	CollaboratorID string
+}
+
+type RemoveCollaboratorHandler struct {
+	coursesRepository coursesRepository
+}
+
+func NewRemoveCollaboratorHandler(repository coursesRepository) RemoveCollaboratorHandler {
+	if repository == nil {
+		panic("coursesRepository is nil")
+	}
+	return RemoveCollaboratorHandler{coursesRepository: repository}
+}
+
+// Handle is RemoveCollaboratorCommand handler.
+// Removes one collaborator from course, returns error.
+func (h RemoveCollaboratorHandler) Handle(ctx context.Context, cmd RemoveCollaboratorCommand) error {
+	return h.coursesRepository.UpdateCourse(ctx, cmd.CourseID, cmd.Teacher, removeCollaborator(cmd))
+}
+
+func removeCollaborator(cmd RemoveCollaboratorCommand) UpdateFunction {
+	return func(_ context.Context, crs *course.Course) (*course.Course, error) {
+		if err := crs.RemoveCollaborator(cmd.Teacher, cmd.CollaboratorID); err != nil {
+			return nil, err
+		}
+		return crs, nil
+	}
+}
