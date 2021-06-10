f477dad4001cb69be70b00cff31f8898c1647129
djerys <kotukhov99@gmail.com>
2021-06-10 17:12:28 +0300

    Add tests for create course command handler

---
+++ internal/coursesorg/app/command/create_course_test.go
@@ -0,0 +1,92 @@
+package command_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+
+	"github.com/authena-ru/courses-organization/internal/coursesorg/app/command"
+	"github.com/authena-ru/courses-organization/internal/coursesorg/app/command/mock"
+	"github.com/authena-ru/courses-organization/internal/coursesorg/domain/course"
+)
+
+func TestCreateCourseHandler_Handle(t *testing.T) {
+	t.Parallel()
+	creator := course.MustNewAcademic("creator-id", course.Teacher)
+	testCases := []struct {
+		Name        string
+		Command     command.CreateCourseCommand
+		ExpectedErr error
+	}{
+		{
+			Name: "create_course",
+			Command: command.CreateCourseCommand{
+				Creator:       creator,
+				CourseStarted: true,
+				CourseTitle:   "Bla Bla Literature",
+				CoursePeriod:  course.MustNewPeriod(2019, 2020, course.FirstSemester),
+			},
+		},
+		{
+			Name: "dont_create_when_zero_creator",
+			Command: command.CreateCourseCommand{
+				CourseStarted: false,
+				CourseTitle:   "Bla Literature",
+				CoursePeriod:  course.MustNewPeriod(2024, 2025, course.SecondSemester),
+			},
+			ExpectedErr: course.ErrZeroCreator,
+		},
+		{
+			Name: "dont_create_when_empty_course_title",
+			Command: command.CreateCourseCommand{
+				Creator:       creator,
+				CourseStarted: true,
+				CoursePeriod:  course.MustNewPeriod(2040, 2041, course.FirstSemester),
+			},
+			ExpectedErr: course.ErrEmptyCourseTitle,
+		},
+		{
+			Name: "dont_create_when_zero_course_period",
+			Command: command.CreateCourseCommand{
+				Creator:       creator,
+				CourseStarted: false,
+				CourseTitle:   "Literature",
+			},
+			ExpectedErr: course.ErrZeroCoursePeriod,
+		},
+		{
+			Name: "dont_create_when_not_teacher_creates_course",
+			Command: command.CreateCourseCommand{
+				Creator:       course.MustNewAcademic("student-id", course.Student),
+				CourseStarted: false,
+				CourseTitle:   "Literature bla",
+				CoursePeriod:  course.MustNewPeriod(2024, 2025, course.SecondSemester),
+			},
+			ExpectedErr: course.ErrNotTeacherCantCreateCourse,
+		},
+	}
+
+	for i := range testCases {
+		c := testCases[i]
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+
+			coursesRepository := mock.NewCoursesRepository()
+			handler := command.NewCreateCourseHandler(coursesRepository)
+
+			courseID, err := handler.Handle(context.Background(), c.Command)
+
+			if c.ExpectedErr != nil {
+				require.Error(t, err)
+				require.True(t, errors.Is(err, c.ExpectedErr))
+				require.Empty(t, courseID)
+				return
+			}
+			require.NoError(t, err)
+			require.NotEmpty(t, courseID)
+			require.Equal(t, coursesRepository.CoursesNumber(), 1)
+		})
+	}
+}
