0ba7431683bd10c39b1b6212b49b650df3c382b5
djerys <kotukhov99@gmail.com>
2021-06-17 19:10:27 +0300

    Add handling of removing student

--- internal/adapter/delivery/http/v1/students.go
+++ internal/adapter/delivery/http/v1/students.go
@@ -1,11 +1,13 @@
 package v1
 
 import (
+	"net/http"
+
+	"github.com/pkg/errors"
+
 	"github.com/authena-ru/courses-organization/internal/adapter/delivery/http/httperr"
 	"github.com/authena-ru/courses-organization/internal/app"
 	"github.com/authena-ru/courses-organization/internal/domain/course"
-	"github.com/pkg/errors"
-	"net/http"
 )
 
 func (h handler) GetAllCourseStudents(w http.ResponseWriter, r *http.Request, courseId string, params GetAllCourseStudentsParams) {
@@ -37,8 +39,29 @@ func (h handler) AddStudentToCourse(w http.ResponseWriter, r *http.Request, cour
 	httperr.InternalServerError("unexpected-error", err, w, r)
 }
 
-func (h handler) RemoveStudentFromCourse(w http.ResponseWriter, r *http.Request, courseId string, studentId string) {
-	w.WriteHeader(http.StatusNotImplemented)
+func (h handler) RemoveStudentFromCourse(w http.ResponseWriter, r *http.Request, courseID string, studentID string) {
+	cmd, ok := unmarshallRemoveStudentCommand(w, r, courseID, studentID)
+	if !ok {
+		return
+	}
+	err := h.app.Commands.RemoveStudent.Handle(r.Context(), cmd)
+	if err == nil {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	if errors.Is(err, app.ErrCourseDoesntExist) {
+		httperr.NotFound("course-not-found", err, w, r)
+		return
+	}
+	if errors.Is(err, course.ErrCourseHasNoSuchStudent) {
+		httperr.NotFound("course-student-not-found", err, w, r)
+		return
+	}
+	if course.IsAcademicCantEditCourseError(err) {
+		httperr.Forbidden("academic-cant-edit-course", err, w, r)
+		return
+	}
+	httperr.InternalServerError("unexpected-error", err, w, r)
 }
 
 func (h handler) AddGroupToCourse(w http.ResponseWriter, r *http.Request, courseId string) {
